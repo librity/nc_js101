5c78e6f540d0c35d427e3c9f02f440a79ca425e0
Luis Geniole <luis.geniole@gmail.com>
2021-12-13 23:55:18 -0300

    Add ticking clock.

---
+++ src/public/js/clock.js
@@ -0,0 +1,20 @@
+const ONE_SECOND = 1000
+
+const clockH2 = document.getElementById('clock')
+
+const padNumber = number => {
+  return String(number).padStart(2, '0')
+}
+
+const updateTime = () => {
+  const now = new Date()
+  const hours = padNumber(now.getHours())
+  const minutes = padNumber(now.getMinutes())
+  const seconds = padNumber(now.getSeconds())
+  const nowTime = `${hours}:${minutes}:${seconds}`
+
+  clockH2.innerText = nowTime
+}
+
+updateTime()
+setInterval(updateTime, ONE_SECOND)
